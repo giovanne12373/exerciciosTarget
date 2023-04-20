814dbdfdd336fc3bcd4041159b1212faed717ba1
giovanne12373 <giovanne.4@hotmail.com>
2023-04-20 16:41:18 -0300

    exercicio 2 atualização

--- exercicio2.cs
+++ exercicio2.cs
@@ -1,21 +1,73 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Newtonsoft.Json;
 
-class Program {
-    static void Main(string[] args) {
-        string json = File.ReadAllText("faturamento.json");
-        decimal[] faturamento = JsonConvert.DeserializeObject<decimal[]>(json);
+namespace Distribuidora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string jsonText = File.ReadAllText(@"C:\giovanne\exercicios\faturamento.json");
+            List<DiaFaturamento> faturamentoDiario = JsonConvert.DeserializeObject<List<DiaFaturamento>>(jsonText);
 
-        decimal menor = faturamento.Min();
-        decimal maior = faturamento.Max();
-        decimal media = faturamento.Where(valor => valor > 0).Average();
+           
+            decimal menorFaturamento = decimal.MaxValue;
+            decimal maiorFaturamento = decimal.MinValue;
 
-        int diasAcimaDaMedia = faturamento.Count(valor => valor > media);
+            foreach (DiaFaturamento dia in faturamentoDiario)
+            {
+                if (dia.Valor > maiorFaturamento)
+                {
+                    maiorFaturamento = dia.Valor;
+                }
 
-        Console.WriteLine($"Menor faturamento: {menor}");
-        Console.WriteLine($"Maior faturamento: {maior}");
-        Console.WriteLine($"Dias acima da média: {diasAcimaDaMedia}");
+                if (dia.Valor < menorFaturamento && dia.Valor > 0)
+                {
+                    menorFaturamento = dia.Valor;
+                }
+            }
+
+            Console.WriteLine($"Menor valor de faturamento: {menorFaturamento:C}");
+            Console.WriteLine($"Maior valor de faturamento: {maiorFaturamento:C}");
+
+            
+            decimal somaFaturamento = 0;
+            int diasComFaturamento = 0;
+
+            foreach (DiaFaturamento dia in faturamentoDiario)
+            {
+                if (dia.Valor > 0)
+                {
+                    somaFaturamento += dia.Valor;
+                    diasComFaturamento++;
+                }
+            }
+
+            decimal mediaFaturamento = somaFaturamento / diasComFaturamento;
+            Console.WriteLine($"Média mensal de faturamento: {mediaFaturamento:C}");
+
+            
+            int diasAcimaDaMedia = 0;
+
+            foreach (DiaFaturamento dia in faturamentoDiario)
+            {
+                if (dia.Valor > mediaFaturamento)
+                {
+                    diasAcimaDaMedia++;
+                }
+            }
+
+            Console.WriteLine($"Número de dias com faturamento acima da média mensal: {diasAcimaDaMedia}");
+        }
+    }
+
+    public class DiaFaturamento
+    {
+        public int Dia { get; set; }
+        public decimal Valor { get; set; }
     }
 }
+
