eac80a76ca56d5ea6ccab2e17572b0d55e6ed863
giovanne12373 <giovanne.4@hotmail.com>
2023-04-20 16:17:52 -0300

    exercicio 1

---
+++ exercicio1.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        int numero = 21; // número a ser verificado
+        bool pertence = PertenceAoFibonacci(number);
+        Console.WriteLine($"O número {number} {(isFibonacci ? "pertence" : "não pertence")} à sequência de Fibonacci.");
+    }
+
+    static bool PertenceAoFibonacci(int number) {
+        int a = 0, b = 1, c = 1;
+        while (c < number) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return (c == number);
+    }
+}
